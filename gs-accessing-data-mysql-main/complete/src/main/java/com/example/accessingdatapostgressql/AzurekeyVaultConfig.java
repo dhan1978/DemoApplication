9bf04e18bc649b39f876593baa787b2eebea4de7
dhan1978 <85505785+dhan1978@users.noreply.github.com>
2021-08-04 15:59:05 +0530

    commit for key vault

--- gs-accessing-data-mysql-main/complete/src/main/java/com/example/accessingdatapostgressql/AzurekeyVaultConfig.java
+++ gs-accessing-data-mysql-main/complete/src/main/java/com/example/accessingdatapostgressql/AzurekeyVaultConfig.java
@@ -1,12 +1,16 @@
 package com.example.accessingdatapostgressql;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
 import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -14,55 +18,83 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import com.azure.core.util.polling.SyncPoller;
 import com.azure.identity.DefaultAzureCredentialBuilder;
-
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
-import com.azure.security.keyvault.secrets.models.DeletedSecret;
-import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
 @Configuration
 @EnableJpaRepositories(basePackages = {
 		"com.example.accessingdatapostgressql" }, entityManagerFactoryRef = "dbEntityManager", transactionManagerRef = "dbTransactionManager")
 
 public class AzurekeyVaultConfig {
+	@Autowired
+	private Environment env;
 	private static final Logger LOGGER = LoggerFactory.getLogger(AzurekeyVaultConfig.class);
 	@Value("${KEY_VAULT_NAME}")
-	 String keyVaultName ;
-	 String keyVaultUri = "https://" + keyVaultName + ".vault.azure.net";
+	private String keyVaultName ;
+	private com.azure.core.util.Configuration config=new com.azure.core.util.Configuration();
+	private  ConcurrentMap<String, String> configurations=new ConcurrentHashMap<>();
+	@Value("${AZURE_CLIENT_ID}")
+	private String AZURE_CLIENT_ID ;
+	
+	@Value("${AZURE_TENANT_ID}")
+	private String AZURE_TENANT_ID ;
+	@Value("${AZURE_CLIENT_SECRET}")
+	private String AZURE_CLIENT_SECRET ;
+	
+	 String keyVaultUri ="https://" + "keyVaultName" + ".vault.azure.net";
+	
+			 //"https://" + "keyVaultName" + ".vault.azure.net";
 	 
 	 SecretClient secretClient;
-	 
+	 /*/-----------------------
+	 public  AzureResourceManager getazureResourceManager() {
+	 AzureResourceManager azureResourceManager = AzureResourceManager.authenticate(
+		        new DefaultAzureCredentialBuilder().build(),
+		        new AzureProfile(AzureEnvironment.AZURE))
+		    .withDefaultSubscription();
+	 return azureResourceManager;
+	 }
+	 */
+	 /*
 	@Bean(name = "secretClient")
 	public  SecretClient getsecretClient() {
+		 if(keyVaultUri==null||keyVaultUri =="") {
+			 keyVaultUri ="https://dhanankeyvault.vault.azure.net";
+		 }
+		 config.put(config.PROPERTY_AZURE_CLIENT_ID, AZURE_CLIENT_ID);
+		 config.put(config.PROPERTY_AZURE_TENANT_ID, AZURE_TENANT_ID);
+		 //config.put(config..PROPERTY_AZURE_TENANT_ID, AZURE_TENANT_ID);
 		LOGGER.info("keyVaultUri", "-------------------------------"+keyVaultUri);
-		
+	//	AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+		//KeyVaultClient keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
 	 secretClient = new SecretClientBuilder()
 		    .vaultUrl(keyVaultUri)
 		    .credential(new DefaultAzureCredentialBuilder().build())
 		    .buildClient(); 
+	 
 	return secretClient;
 	}
+	*/
 	@Bean(name = "dbDataSource")
 	
 	public DataSource dbDataSource() {
-		secretClient=getsecretClient();
+		//secretClient=getsecretClient();
 		LOGGER.info("++++++++++++++++++++++++++++++++++++ FDATA ++++++++++++++++++++++++++++++++++++");
-		LOGGER.info("driver class: [{}]", secretClient.getSecret("spring-datasource-driver-class-name").getValue());
+		LOGGER.info("driver class: [{}]", env.getProperty("spring-datasource-driver-class-name"));
 		
-		LOGGER.info("url class: [{}]", secretClient.getSecret("spring-datasource-url").getValue());
+		LOGGER.info("url class: [{}]", env.getProperty("spring-datasource-url"));
 		
-		LOGGER.info("username: [{}]", secretClient.getSecret("spring-datasource-username").getValue());
+		LOGGER.info("username: [{}]", env.getProperty("spring-datasource-username"));
 		
-		LOGGER.info("password: [{}]", secretClient.getSecret("spring-datasource-password").getValue());
+		LOGGER.info("password: [{}]", env.getProperty("spring-datasource-password"));
 		
 		LOGGER.info("------------------------------------ DATA ------------------------------------");
 		secretClient.getSecret("dhanankeyvault");
 	DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	dataSource.setDriverClassName(secretClient.getSecret("spring-datasource-driver-class-name").getValue());
-	dataSource.setUrl(secretClient.getSecret("spring-datasource-url").getValue());
-	dataSource.setUsername(secretClient.getSecret("spring-datasource-username").getValue());
-	dataSource.setPassword(secretClient.getSecret("spring-datasource-password").getValue());
+	dataSource.setDriverClassName(env.getProperty("spring-datasource-driver-class-name"));
+	dataSource.setUrl(env.getProperty("spring-datasource-url"));
+	dataSource.setUsername(env.getProperty("spring-datasource-username"));
+	dataSource.setPassword(env.getProperty("spring-datasource-password"));
 	return dataSource;
 }
 	
