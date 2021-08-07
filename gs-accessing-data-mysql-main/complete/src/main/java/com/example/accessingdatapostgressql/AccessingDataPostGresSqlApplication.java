c08acf1b73c69d1d22a5d32fe215ddd177a91882
dhan1978 <85505785+dhan1978@users.noreply.github.com>
2021-08-08 01:13:52 +0530

    updated poc

--- gs-accessing-data-mysql-main/complete/src/main/java/com/example/accessingdatapostgressql/AccessingDataPostGresSqlApplication.java
+++ gs-accessing-data-mysql-main/complete/src/main/java/com/example/accessingdatapostgressql/AccessingDataPostGresSqlApplication.java
@@ -1,12 +1,39 @@
 package com.example.accessingdatapostgressql;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+//----------------------------------------------------
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
+
+
+@ComponentScan
+@Configuration
 @SpringBootApplication
-public class AccessingDataPostGresSqlApplication {
-
+public class AccessingDataPostGresSqlApplication //implements CommandLineRunner
+{
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AccessingDataPostGresSqlApplication.class, args);
+		
 	}
+/*
+	 @Override
+	    public void run(String... args) {
+	        System.out.println("property your-property-name value is: " + mySecretProperty);
+	    }
+	*/
+	 
 }
